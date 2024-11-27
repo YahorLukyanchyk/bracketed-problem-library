a8e2a510e6acab017e8845efc804f4825de3e562
Yahor Lukyanchyk <yahorlukyanchyk@gmail.com>
2024-11-27 10:53:18 +0300

    changed logic of core BracketsChecker class

--- src/BracketsChecker.php
+++ src/BracketsChecker.php
@@ -4,13 +4,12 @@ namespace BracketsChecker;
 
 require 'vendor/autoload.php';
 
-use Exception;
 use BracketsChecker\Interfaces\BracketsCalculatorInterface;
 use InvalidArgumentException;
 
 class BracketsChecker implements BracketsCalculatorInterface
 {
-    public function check(string $inputData, string $filePath): bool
+    public function check(string $inputData): bool
     {
         /* Check if input string contains ivalid values */
         if (preg_match('/^[() \n\t\r]*$/', $inputData) === 0) {
@@ -19,37 +18,7 @@ class BracketsChecker implements BracketsCalculatorInterface
 
         $inputData = preg_replace('/[^()]/', '', $inputData);
 
-        /* Get data from file */
-        $fileData = file_get_contents($filePath);
-
-        /* Check if file was successfuly opened and path is correct */
-        if (!$fileData) {
-            throw new Exception("File not found.");
-        }
-
-        $fileData = preg_replace('/[^()]/', '', $fileData);
-
-        /* Calculate brackets for each data sources */
-        $fileDataBracketsCount = [
-            'openingBrackets' => substr_count($fileData, '('),
-            'closingBrackets' => substr_count($fileData, ')')
-        ];
-
-        $inputDataBracketsCount = [
-            'openingBrackets' => substr_count($inputData, '('),
-            'closingBrackets' => substr_count($inputData, ')')
-        ];
-
-        /* Print counted brackets stats in the CLI */
-        echo "Bracket Sequence from input:\n" .
-            "Opening brackets - " . $inputDataBracketsCount['openingBrackets'] . "\n" .
-            "Closing brackets - " . $inputDataBracketsCount['closingBrackets'] . "\n" .
-            "Bracket Sequence from file:\n" .
-            "Opening brackets - " . $fileDataBracketsCount['openingBrackets'] . "\n" .
-            "Closing brackets - " . $fileDataBracketsCount['closingBrackets'] . "\n";
-
-        /* Return False if the amount of brackets is not the same */
-        if ($fileData !== $inputData) {
+        if (substr_count($inputData, "(") !== substr_count($inputData, ")")) {
             return false;
         }
 
